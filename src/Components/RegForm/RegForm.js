ac20d96b784dcc1b73872303f00ba17527825b53
Silas Hallahan <silasishallahan@gmail.com>
2019-07-29 17:41:24 -0700

    deleted stuff

---
+++ src/Components/RegForm/RegForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class RegForm extends React.Component {
+  render() {
+    return(
+      <form className='reg-form' id='regform'>
+        
+      </form>
+    )
+  }
+}
---
+++ src/Routes/Registration/Registration.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Registration extends React.Component {
+  render() {
+    return(
+      <section className='reg-area'>
+        
+      </section>
+    )
+  }
+}
